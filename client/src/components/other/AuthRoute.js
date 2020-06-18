9a617803455fda01405b70ab57de5754c0669b79
AhmedAdelFahim <ahmedadelfahim@gmail.com>
2020-06-18 18:26:39 +0200

    deployment preparation

--- client/src/components/other/AuthRoute.js
+++ client/src/components/other/AuthRoute.js
@@ -3,9 +3,13 @@ import { connect } from "react-redux";
 import { Redirect, Route } from "react-router-dom";
 
 const AuthRoute = (props) => {
+    console.log(props)
     const { isAuthUser, type } = props;
     if (type === "guest" && isAuthUser) return <Redirect to="/home" />;
-    else if (type === "private" && !isAuthUser) return <Redirect to="/" />;
+    else if (type === "private" && !isAuthUser) {
+        console.log("done")
+        return <Redirect to="/"/>;
+    }
 
     return <Route {...props} />;
 };
