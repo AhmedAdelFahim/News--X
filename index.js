64b38ff426ef9986247719a1f01d00c0680c2448
AhmedAdelFahim <ahmedadelfahim@gmail.com>
2020-06-18 16:54:18 +0200

    deployment preparation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 // const cors = require("cors");
+const path = require('path')
 require('./db/mongooes');
 const authRouter = require('./routes/auth')
 const userRouter = require('./routes/user')
