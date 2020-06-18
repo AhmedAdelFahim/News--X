3906c5fd8e110356bc6ca604c73b55670f214c8b
AhmedAdelFahim <ahmedadelfahim@gmail.com>
2020-06-18 22:55:58 +0200

    deployment preparation

--- models/User.js
+++ models/User.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const uniqueArrayPlugin = require('mongoose-unique-array');
 const saltRounds = 10;
 
 const userSchema = new mongoose.Schema({
